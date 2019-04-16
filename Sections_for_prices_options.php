2af4e2a896b092462e8df62598f5183353b4de0b
alanpereyra57 <alan@pereyra.online>
2019-04-15 22:51:15 -0300

    initial commit

---
+++ Sections_for_prices_options.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: Woocommerce Levels by Informatica Pereyra
+* Plugin URI: https://informatica.pereyra.online/
+* Description: This description will be update.
+* Version: 0.0.1
+* Author: Alan Pereyra
+* Author URI: https://informatica.pereyra.online/alanpereyra57
+**/
