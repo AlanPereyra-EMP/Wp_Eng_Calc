07d80aa04de08c2f7635b016031390f4293055c8
alanpereyra57 <alan@pereyra.online>
2019-05-25 16:41:24 -0300

    add 'mm' to end of pitagoras result

--- includes/wec.js
+++ includes/wec.js
@@ -3,5 +3,5 @@ function calcTest(){
     var a = parseInt(document.calc.a.value);
     var b = parseInt(document.calc.b.value);
     var result = (a**2)+(b**2);
-    document.calc.c.value = Math.sqrt(result);
+    document.calc.c.value = Math.sqrt(result)+' mm';
 }
